667d1e673686579aaf2fcd77a574bace3852b678
RobinD57 <robin.davidseth@gmail.com>
2020-05-25 16:39:09 +0200

    finished listings controller, changed routes and created devise view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,14 +7,32 @@ class ListingsController < ApplicationController
 
   def show; end
 
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    raise
+    @listing = Listing.new(listing_params)
+
+    respond_to do |format|
+      if @listing.save
+        format.html { redirect_to @listing, notice: 'listing was successfully created.' }
+        format.json { render :show, status: :created, location: @listing }
+      else
+        format.html { render :new }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_listing
     @listing = Listing.find(params[:id])
   end
 
-
   def listing_params
-    params.require(:listing).permit(:name, :photo)
+    params.require(:listing).permit(:name, :address, :price, :type, :capacity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :listing
+  resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
