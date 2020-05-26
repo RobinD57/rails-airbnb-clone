7aa187692b12be52da09d8d28102c5d927409cf6
RobinD57 <robin.davidseth@gmail.com>
2020-05-26 15:37:50 +0200

    added review controller, pundit functionality

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,59 @@
+class ReviewsController < ApplicationController
+  before_action :set_listing, only: [:new, :create]
+  before_action :set_review, only: [:destroy, :edit, :update]
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    @reviews = policy_scope(Review)
+  end
+
+  def new
+    @review = Review.new
+    authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.listing = @listing
+    @review.user = current_user
+    authorize @listing
+    respond_to do |format|
+      if @listing.save
+        format.html { redirect_to @listing, notice: 'listing was successfully created.' }
+        format.json { render :show, status: :created, location: @listing }
+      else
+        format.html { render :new }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @review.update(review_params)
+      redirect_to @review, notice: 'Review was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to listings_url, notice: 'Listing was successfully destroyed.'
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:user_id, :content, :rating, :listing_id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+end
