9ed9de6ff1ecaecefdb52b61f94a9a0815582166
RobinD57 <robin.davidseth@gmail.com>
2020-05-27 12:43:20 +0200

    started booking controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :listings do
     resources :reviews, shallow: true
+    resources :bookings, only: [:create]
   end
+  resources :bookings, only: [:index, :show, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
